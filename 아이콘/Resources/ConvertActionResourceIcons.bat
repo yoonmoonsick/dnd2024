47d620aabe6b3fbf1a5645d8e701336c2ad50641
Yoonmoonsick <yoonmoonsick@icloud.com>
2025-06-23 05:33:29 +0900

    fix: Correct output variable usage in ConvertActionResourceIcons.bat

--- 아이콘/Resources/ConvertActionResourceIcons.bat
+++ 아이콘/Resources/ConvertActionResourceIcons.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal enabledelayedexpansion
+
 mkdir "0_Controller_80"
 for %%f in (*.dds) do (
     magick "%%f" -resize 80x80 "0_Controller_80\%%~nf.png"
@@ -12,16 +14,16 @@ for %%f in (*.dds) do (
 mkdir "2_Highlight_48"
 set "source=%cd%"
 set "destination=%cd%\2_Highlight_48"
+
 for %%f in (*.dds) do (
     set "filename=%%~nf"
-    set "output=%destination%\!filename!.png"
+    set "output=!destination!\!filename!.png"
 
     rem 밝기 25% 증가 + glow 효과 적용
     magick ^
     ^( "%%f" -resize 48x48 -modulate 125,100,100 ^) ^
     ^( -clone 0 -blur 0x16 -evaluate multiply 0.5 ^) ^
-    -compose screen -composite ^
-    "!output!"
+    -compose screen -composite "!output!"
 )
 
 mkdir "3_Missing_48"
